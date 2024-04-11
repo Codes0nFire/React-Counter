c477b5937cb2bcfb1edde9c578f1492482a791be
NoN Responsive WebDeveloper <gg.georgemail50a@gmail.com>
2024-04-11 12:45:48 +0530

    added  task completion and task count functionality

--- reactTodo/src/App.jsx
+++ reactTodo/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   
   
 var [title,settitle]= useState("");
-var [task,settask] =useState("");
+var [task,settask] =useState([]);
 
 
 //handleing The Submit functionality
@@ -20,15 +20,28 @@ function submitHandler(event){
 
   settask([...task,newTask]);
 
+}
 
-  // console.log(task);
+//checkboxHandler
+
+function checkboxHandler(e,i){
+ 
+
+  e.target.classList.toggle(`bg-green-500`);
+  e.target.classList.toggle(`border`);
+  e.target.nextSibling.classList.toggle(`line-through`);
+
+  let copyTasks = [...task];
+   copyTasks[i].completed = !task[i].completed;
+        settask(copyTasks);
 
 
-  
+        // console.log(task);
 
 }
 
-
+let doneTask=task.filter((t) => t.completed === true).length
+// console.log(doneTask)
 
 
 var taskRender= <h1>ADD SOME TASKS </h1> 
@@ -41,7 +54,10 @@ if(task.length>0){
     return (
        <li key={index} className=" text-2xl shadow-lg bg-slate-700 leading-6 px-4 py-2 my-3
         flex items-center justify-between gap-8" >
-        <p className="font-light text-lg " >{task.title.toLowerCase()}</p>
+       <div className="flex items-center gap-2">
+        <div onClick={(e)=>checkboxHandler(e,index)} className="checkbox w-6 h-6 rounded-full border"></div>
+       <p className="font-light text-xl " >{task.title.toUpperCase()}</p>
+       </div>
         <div>
         <i className=" text-lg ri-pencil-line px-2 cursor-pointer"></i>
         <i className= " text-lg ri-delete-bin-6-fill cursor-pointer "></i>
@@ -59,7 +75,7 @@ if(task.length>0){
     <div 
      className="    flex gap-5 items-center justify-center w-screen h-screen bg-slate-600 flex-col text-white" >
 
-      <h1 className="text-white text-8xl" >Todo</h1>
+      <h1 className="text-white text-8xl" >Todo {doneTask}/{task.length}</h1>
 
       <form onSubmit={submitHandler} className="flex gap-3 flex-col items-center justify-center" >
         <input
